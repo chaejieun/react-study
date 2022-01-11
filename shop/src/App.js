95b812c7e649e25f465bd905daff050c0753edb2
jechae <jechae@creativehill.co.kr>
2022-01-11 17:26:04 +0900

    react-router-dom 활용법 & 기본 라우팅 세팅

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,8 +1,10 @@
+/* eslint-disable */
 import React, { useState } from "react";
 import { Navbar, Container, Nav, NavDropdown, Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Data from "./data.js";
+import { Link, Route, Switch } from "react-router-dom";
 
 function App() {
   let [shoes, shoes변경] = useState(Data);
@@ -34,26 +36,77 @@ function App() {
           </Navbar.Collapse>
         </Container>
       </Navbar>
-      <div className="background">
-        <h1>20% Season off</h1>
-        <p>this is product explanatin area</p>
-        <Button>test</Button>
-      </div>
 
-      <div className="container">
-        <div className="row">
-          {shoes.map((shoes) => (
-            <div className="col-md-4">
-              <img src={shoes.img} width="100%" alt="" />
-              <h4> {shoes.title} </h4>
-              <p>
-                {shoes.content} & {shoes.price}
-              </p>
-            </div>
-          ))}
+      <Route exact path="/">
+        <div>메인페이지에요</div>
+
+        <div className="background">
+          <h1>20% Season off</h1>
+          <p>this is product explanatin area</p>
+          <Button>test</Button>
         </div>
-      </div>
+
+        <div className="container">
+          <div className="row">
+            {shoes.map((a, i) => {
+              return <Card shoes={shoes[i]} i={i} key={i} />;
+            })}
+          </div>
+        </div>
+      </Route>
+      <Route path="/detail">
+        <div>디테일 페이지에요</div>
+
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <img
+                src="https://codingapple1.github.io/shop/shoes1.jpg"
+                width="100%"
+              />
+            </div>
+            <div className="col-md-6 mt-4">
+              <h4 className="pt-5">상품명</h4>
+              <p>상품설명</p>
+              <p>120000원</p>
+              <button className="btn btn-danger">주문하기</button>
+            </div>
+          </div>
+        </div>
+      </Route>
+      {/* '/' 경로로 들어왔을 때와, '/detail'로 들어왔을 때에도 메인페이지 내용이 보이는 이유는?
+          '/' <- 이 슬래쉬가 메인에도 포함되어 있기 때문에 '메인페이지에요 와 디테일 페이지에요'가 함께 보이는 이유!
+          ----> 그것을 막기 위해서는 [exact]라는 것을 넣으면, 무조건 문자가 일치하는 것만 타게 됌
+     */}
+
+      {/*  <Route path="/어쩌구" component={Modal}></Route> 
+        /어쩌구 경로로 들어오게 될 경우
+        component-> 'Modal'이라는 것을 바로 실행시킬 수 있도록 처리
+      */}
+
+      {/*  
+        URL에 맞는 HTML을 갈아치우는 걸 흉내내는 것이기 때문에 서로 다른 HTML이 아님!
+      */}
     </>
+  );
+}
+
+function Card(props) {
+  return (
+    <div className="col-md-4">
+      <img src={props.shoes.img} width="100%" alt="" />{" "}
+      <img
+        src={
+          "https://codingapple1.github.io/shop/shoes" + (props.i + 1) + ".jpg"
+        }
+        width="100%"
+        alt=""
+      />
+      <h4> {props.shoes.title} </h4>
+      <p>
+        {props.shoes.content} & {props.shoes.price}
+      </p>
+    </div>
   );
 }
 
--- shop/src/index.js
+++ shop/src/index.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+// router setting
+//import * as serviceWorker from "./serviceWorker";
+
+import { BrowserRouter } from "react-router-dom"; // react-router-dom 초기 셋팅법 (/abc로 이동해주세요 ( #기호 작동하지 않음))
+// import { HashRouter } from "react-router-dom";  // react-router-dom 초기 셋팅법
+// (HashRouter로도 사용 가능함 (#기호 작동 -> #기호는 서버에 절대 전송되지 않는다. react가 안정적으로 처리해줄 수 있음 ))
+// Browser와 HashRouter의 차이
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
